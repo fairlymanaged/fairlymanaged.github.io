8fb21e9dcd9ae2ff2c95f90c8c5b4be65b1ad449
fairlymanaged <fairlymanaged@gmail.com>
2024-11-26 01:01:21 -0500

    update friend-listener

--- friends/friend-listener.js
+++ friends/friend-listener.js
@@ -2,8 +2,12 @@ import watch from "node-watch";
 import shell from "shelljs";
 
 watch("./friends/friend-images", { recursive: true }, (evt, name) => {
-    if (shell.exec('git add -A').code == 0) {
-        shell.exec('git commit -m "Friend images updated"');
+    if (shell.exec('git add -A friends/friend-images/\*').code == 0) {
+        if(shell.exec('git commit -m "Friend images updated"').code == 0) {
+            shell.exec('git push');
+        } else {
+            console.log("Error committing files");
+        }
     } else {
         console.log("Error adding files");
     }
